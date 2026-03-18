1ed592092e510ea1e60b7be1d4963ae42689fe43
Milan Javiya <milanjaviya971@gmail.com>
2026-03-18 06:17:54 -0700

    chore: ignore and remove build artifacts

--- .next-employer/dev/server/chunks/ssr/_next-internal_server_app_employer_jobs_page_actions_1be61050.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/employer/jobs/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_employer_jobs_page_actions_1be61050.js.map
--- .next-employer/dev/static/chunks/src_app_page_tsx_d7502604._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_318a79d1._.js",
-  "static/chunks/node_modules_react-icons_rx_index_mjs_03f9ea46._.js",
-  "static/chunks/node_modules_react-icons_io5_index_mjs_9bce8f6c._.js"
-],
-    source: "dynamic"
-});
